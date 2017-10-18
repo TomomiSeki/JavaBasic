efea6fb158aaef01cae8bd0e32da15e020aa0e67
Tomomi <lotusandsakura666@gmail.com>
2017-10-18 16:22:17 +0900

    PTractice02_10まで完了

--- Practice/src/practice02/PTra02_10.java
+++ Practice/src/practice02/PTra02_10.java
@@ -21,6 +21,12 @@ public class PTra02_10 {
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
 		 * ※ プログラムは何行書いても良いです
 		 */
+		coffee*=2*1.08;
+		snack*=1.08;
+		moneyInHand-=coffee+snack;
+		System.out.println(moneyInHand);
+
+
 
 	}
 }
