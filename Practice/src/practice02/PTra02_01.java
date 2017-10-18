1a1035f46402290181f14cf5d0d97d18b63a5403
Tomomi <lotusandsakura666@gmail.com>
2017-10-18 15:47:00 +0900

    PTra02_05まで完了

--- Practice/src/practice02/PTra02_01.java
+++ Practice/src/practice02/PTra02_01.java
@@ -10,13 +10,17 @@ public class PTra02_01 {
 	public static void main(String[] args) {
 
 		// ★ 20を5で割った値を出力してください
-
+		int i=20/5;
+		System.out.println(i);
 
 		// ★ 126に13を掛けた値を出力してください
-
+		int j;
+		j=126*13;
+		System.out.println(j);
 
 		// ★ 97を6で割ったときの余りを出力してください
-
+		int k=97%6;
+		System.out.println(k);
 
 	}
 }
