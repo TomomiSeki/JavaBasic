36f5c271cd2c236ac65f47dfcaefb7c71b42eb49
Tomomi <lotusandsakura666@gmail.com>
2017-10-18 15:34:27 +0900

    PTra01_07まで完了

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,8 +21,14 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
+		bo=true;
+		by=12;
+		s=1000;
+		c='え';
+		i=102;
+		l=10000000;
+		f=10.3f;
+		d=52.34;
 
 
 
@@ -31,8 +37,14 @@ public class PTra01_07 {
 
 
 		// ★ それぞれの変数の中身を出力してください
-
-
+		 System.out.println(bo);
+		 System.out.println(by);
+		 System.out.println(s);
+		 System.out.println(c);
+		 System.out.println(i);
+		 System.out.println(l);
+		 System.out.println(f);
+		 System.out.println(d);
 
 
 
